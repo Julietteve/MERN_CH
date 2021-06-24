d0fef11ef0cc7fff0476c3819a98957f51345e7b
Julieta Garcia <nmaglio>
2021-06-24 17:24:08 -0300

    added  server

--- desafio-5/index.js
+++ desafio-5/index.js
@@ -1,1 +1,23 @@
-console.log(12+50)
+const http = require('http');
+require('dotenv').config()
+
+const server = http.createServer((petition,res)=>{
+
+   /*  Math.floor(Math.random() * (1 + High - Low)) + Low */
+   
+    let id = Math.floor(Math.random() * 10) + 1;
+    let price = +(Math.random()* (999.99 - 0.00) + 0.00).toFixed(2)
+    
+    let data = {
+        id: id,
+        title: `Producto ${id}`,
+        price: price,
+        thumbnail: `Foto ${id}`,
+    }
+
+    res.end(JSON.stringify(data))
+})
+
+server.listen( process.env.PORT , ()=>{
+    console.log("Server listening on port " +  process.env.PORT)
+})
