ed918dc9ebb1229cba1814bb44a6d37103711432
Julieta Garcia <nmaglio>
2021-07-03 19:46:04 -0300

    fix 404

--- desafio-7/index.js
+++ desafio-7/index.js
@@ -8,29 +8,29 @@ let counterItems=0;
 let counterItem=0;
 
 
-app.get('/items', async (req,res)=>{
+app.get('/items', async (req,res) => {
     const data = await getItems()
     counterItems++;
     res.send(data)
 })
 
-app.get('/item-random', async(req,res)=>{
+app.get('/item-random', async(req,res) => {
     const data = await getRandomItem()
     counterItem++;
     res.send(data)
 })
 
-app.get('/visitas', (req, res)=>{
+app.get('/visitas', (req, res) => {
     const obj = Object.assign({},{visitas: {items :counterItems, item: counterItem}})
     res.send(obj)
 })
 
 app.get('*', (req, res)=>{
-    res.send('404 | Ruta no encontrada')
+    res.send( '404 | Ruta no encontrada');
 })
 
 
 const server = app.listen(PORT, ()=> {
-    console.log(`Servidor escuchando en puerto ${server.address().port}`)
+    console.log(`Servidor escuchando en puerto http://localhost:${server.address().port}`)
 })
 server.on( "error" , err => console.log(`Error en el servidor :  ${err}`))
--- desafio-7/utils.js
+++ desafio-7/utils.js
@@ -3,12 +3,12 @@ const { readFile } = require('fs/promises');
 const getItems = async () =>{
 
     const data = await readFile('./productos.txt', 'utf-8')
-    const obj = Object.assign({},{item: JSON.parse(data)}, {cantidad:JSON.parse(data).length})
+    const obj = Object.assign({},{items: JSON.parse(data)}, {cantidad:JSON.parse(data).length})
 
     return obj
 }
 
-const getRandomItem = async ()=> {
+const getRandomItem = async () => {
 
     const data = await readFile('./productos.txt', 'utf-8')
     const randomIndex = Math.floor(Math.random() * JSON.parse(data).length)
